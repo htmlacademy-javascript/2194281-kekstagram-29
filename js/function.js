5176a81b80d40a2e9f1c2bc5ed778e1f4381427d
Zakhar Sherstnev <zakharsherstnev@MacBook-Pro-Zakhar.local>
2023-06-30 11:20:22 +0300

    func

--- js/function.js
+++ js/function.js
@@ -1,8 +1,21 @@
-function lengthSringCalculate(stringCalculate,maxlengthSthing) {
-  if(stringCalculate < maxlengthSthing) {
+function lengthSringCalculate(stringCalculate, maxlengthSthing) {
+  if (stringCalculate < maxlengthSthing) {
     console.log('Ok');
   }
   console.log('don`t ok');
 }
+
 lengthSringCalculate('lalala', 20);
+
+
+let recycling = false;
+const workingDayLangth = (startWork, finishWork, meeting, meetingLength) => {
+  if ((meeting + meetingLength) > finishWork) {
+    recycling = true;
+
+  }
+};
+
+workingDayLangth('08:00', '17:30', '17:00', 90);
+console.log(recycling);
 
