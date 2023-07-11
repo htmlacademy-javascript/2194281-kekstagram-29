4900af4639ca76d9511e7c31b655f562edabb477
Zakhar Sherstnev <zakharsherstnev@MacBook-Pro-Zakhar.local>
2023-07-12 00:49:22 +0300

    открывается и закрывается часть 1

--- js/getMiniImg.js
+++ js/getMiniImg.js
@@ -1,17 +1,26 @@
+// импортирую иммитацию сервера (заглушку)
 import {newArrayFhotoOptions} from './seeders/seeders.js';
 
+// содержимое темплейта
 const pictures = document.querySelector('.pictures');
+// добавляю темплейт в разметку
 const pictureMinTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
+// записываю массив сидер в переменную
 const picturesImg = newArrayFhotoOptions;
 
+// цикл клонирует темплейт и добавляет в него данные
 picturesImg.forEach( (picturesImg) => {
+  // клонирую
   const pictureElement = pictureMinTemplate.cloneNode(true);
+
+  //переписываю данные из массива сидера
   pictureElement.querySelector('.picture__img').src = picturesImg.url;
   pictureElement.querySelector('.picture__img').alt = picturesImg.description;
   pictureElement.querySelector('.picture__likes').textContent = picturesImg.likes;
   pictureElement.querySelector('.picture__comments').textContent = picturesImg.comments.length;
 
+// добавляю в разметку клоны темплейта
   pictures.appendChild(pictureElement);
 });
 
